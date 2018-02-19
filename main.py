4866b92c0da234590114bd88278f47a5a72c42c6
JamesRaynor <hongjiaminghjm@gmail.com>
2018-02-19 22:41:11 +0800

    实现自动进入房间及开始游戏功能

--- main.py
+++ main.py
@@ -5,31 +5,68 @@ from util import RGB2Int
 from util import Int2RGB
 from util import click
 from util import display_room_rect
+from util import enter_room_from_lobby
+from util import click_start
+from util import exit_room
 from solve import solve_game_by_brute_force
+from enum import Enum
 
+class GameState(Enum):
+	GS_INGAME = 1 #处在游戏中
+	GS_OVER = 2 #游戏超时，等待其他玩家完成消除并结束一场游戏
+	GS_INROOM = 3 #在房间里等待游戏开始
+	GS_CLOSED = 4 #游戏窗口被关闭（主动关闭或者被踢出房间
+	GS_MINIMIZED = 5 #游戏窗口被最小化（无法获取颜色值）
+	GS_INLOBBY = 6 # 在游戏大厅中
+	GS_UNKNOWN = 7 #未定义状态(这是错误)
 
-def enter_room_from_lobby():
-	# 由于未知原因，对游戏大厅窗口调用 win32gui.GetPixel 会得到错误
-	# 经过考量，觉得游戏大厅可以全屏而固定位置，且只需要模拟一个“快速加入游戏”
-	# 的按钮即可，故直接编码模拟点击按钮所在x，y坐标
-	# 注意需要保证游戏大厅最大化，且屏幕大小更换后此函数无法正常工作
-	hWnd = win32gui.FindWindow(None, "连连看")
-	win32gui.SetForegroundWindow(hWnd)
-	win32gui.SetActiveWindow(hWnd)	
-	click(hWnd, "enterRoom")
-	hWnd = win32gui.FindWindow(None, "QQ游戏 - 连连看角色版")
-	if hWnd == 0:
-		print("进入游戏房间失败，退出程序")
-		exit()
 
 def debug():
 	print("In debug")
 	# display_room_rect(3,3,100,30)
 	enter_room_from_lobby()
 	# hWnd = win32gui.FindWindow(None, "连连看")
-	
-def main():
-	solve_game_by_brute_force()
+
+def get_status():
+	pass
+	return 0
 
 if __name__ == '__main__':
-	solve_game_by_brute_force()
+	# 这个状态机实际上不健壮，万一出现强制前置弹窗等意外状态，程序即陷入错误
+	status = GameState.GS_INLOBBY
+	waitting_time = 0
+	while True:
+		if status == GameState.GS_INLOBBY:
+			enter_room_from_lobby()
+		elif status == GameState.GS_INROOM:
+			if waitting_time == 0:
+				click_start()				
+			elif waitting_time < 20:
+				time.sleep(1)
+				waitting_time += 1
+				print("等待{}秒...".format(waitting_time))
+			else:
+				exit_room()
+		elif status == GameState.GS_INGAME:
+			solve_game_by_brute_force()
+			exit_room()
+
+		pre_status = status
+		status = get_status()
+		# 退出room内等待状态后清零计时
+		if pre_status == GameState.GS_INROOM and status != GameState.GS_INROOM:
+			waitting_time = 0
+		# 遇到未知情况时尝试重新恢复窗口焦点恢复运行，无法保证必然有效
+		if status == GameState.GS_UNKNOWN:
+			hWnd = win32gui.FindWindow(None, "QQ游戏 - 连连看角色版")
+			if hWnd == 0:
+				hWnd = win32gui.FindWindow(None, "连连看")
+				assert hWnd != 0
+				win32gui.SetForegroundWindow(hWnd)
+				win32gui.SetActiveWindow(hWnd)
+				status = GameState.GS_INLOBBY
+				continue
+			else:
+				win32gui.SetForegroundWindow(hWnd)
+				win32gui.SetActiveWindow(hWnd)
+				status = GameState.GS_INROOM
--- util.py
+++ util.py
@@ -6,6 +6,8 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
+g_lobby_hWnd = 0
+
 def RGB2Int(red, green, blue):
 	return red + green * 256 + blue * 256 * 256
 
@@ -15,15 +17,40 @@ def Int2RGB(rgb):
 	blue = rgb & 255
 	return (red, green, blue)
 
+def enter_room_from_lobby():
+	# 20180219
+	# 因为有title为 连连看 的隐藏窗口存在导致之前
+	# 无法getPixel即发送鼠标信息失败问题
+	# 我们需要获取visible的title为 连连看 的窗口
+	hWnd = get_lobby_hWnd()
+	win32gui.SetForegroundWindow(hWnd)
+	win32gui.SetActiveWindow(hWnd)
+	click(hWnd, "enterRoom")
+	hWnd = win32gui.FindWindow(None, "QQ游戏 - 连连看角色版")
+	if hWnd == 0:
+		print("进入游戏房间失败，退出程序")
+		exit()
+
+def exit_room():
+	hWnd = win32gui.FindWindow(None, "QQ游戏 - 连连看角色版")
+	win32gui.SetForegroundWindow(hWnd)
+	win32gui.SetActiveWindow(hWnd)
+	click(hWnd, "exitRoom")
+
+def click_start():
+	hWnd = win32gui.FindWindow(None, "QQ游戏 - 连连看角色版")	
+	win32gui.SetForegroundWindow(hWnd)
+	win32gui.SetActiveWindow(hWnd)
+	click(hWnd, "clickStart")
+
 def click(hWnd, action):
 	delay_time = 0.1
 	if action == "enterRoom":
+		# x, y = -100, -80
 		x, y = 270, 152
 		delay_time = 3
-	elif action == "clickPractice":
-		x, y = 650, 200
 	elif action == "clickStart":
-		x, y = 650, 200
+		x, y = 650, 570
 	elif action == "exitRoom": # 点右上角的乂，退出房间
 		x, y = 780, 10
 	elif action == "resort": # 重排
@@ -33,7 +60,9 @@ def click(hWnd, action):
 		print("Wrong action string input, exiting...")
 		exit()
 	print("Clicked {} and {}".format(x,y))
-	win32api.SetCursorPos((x, y)) # important to make click valide
+	# win32api.SetCursorPos((x, y)) # important to make click valide
+	print(win32gui.ClientToScreen(hWnd, (x, y)))
+	win32api.SetCursorPos(win32gui.ClientToScreen(hWnd, (x, y))) # important to show where is acturally clicked
 	lParam = win32api.MAKELONG(x, y) 
 	win32gui.SendMessage(hWnd, win32con.WM_LBUTTONDOWN, 0, lParam)
 	win32gui.SendMessage(hWnd, win32con.WM_LBUTTONUP, 0, lParam)
@@ -62,24 +91,31 @@ def display_room_rect(left_top_x, left_top_y, right_bot_x, right_bot_y):
 	plt.imshow(image[..., ::-1]) # GetPixel得到的是BGR而不是RGB，所以需要转换一下
 	plt.show()	
 
+def enumHandler(hWnd, lParam):
+	if win32gui.IsWindowVisible(hWnd) and win32gui.GetWindowText(hWnd) == "连连看":
+		global g_lobby_hWnd
+		g_lobby_hWnd = hWnd
+
+def get_lobby_hWnd():
+	# 在enumHandler中设置了g_lobby_hWnd作为返回值
+	win32gui.EnumWindows(enumHandler, None)	
+	# print("g_lobby_hWnd == {}".format(g_lobby_hWnd))
+	# hDC = win32gui.GetWindowDC(g_lobby_hWnd)	
+	# rect = win32gui.GetWindowRect(g_lobby_hWnd)
+	# x = rect[0]
+	# y = rect[1]
+	# w = rect[2] - x
+	# h = rect[3] - y
+	# print("Window name: " + win32gui.GetWindowText(g_lobby_hWnd))
+	# print("Window hDC: " + str(hDC))
+	# print("x, y, w, h == " + str(x) + " " + str(y) + " " + str(w) + " " + str(h) + " ")
+	# print()
+	return g_lobby_hWnd
+
 def debug():
 	print("In debug util.py")
-	colorInt = 16711680 # left red
-	# colorInt = 65280 # mid green
-	# colorInt = 255 # right blue
-	
-	print(Int2RGB(colorInt))
-	import matplotlib.pyplot as plt
-	import numpy as np
-	image = np.zeros((10, 10, 3))
-	for y in range(10):
-		for x in range(10):
-			image[y, x] = np.asarray(Int2RGB(colorInt))/255
-	print(image.shape)
-	plt.imshow(image)
-	plt.show()
-	print(image)
-
+	enter_room_from_lobby()
+	click_start()
 
 if __name__ == '__main__':
 	debug()
